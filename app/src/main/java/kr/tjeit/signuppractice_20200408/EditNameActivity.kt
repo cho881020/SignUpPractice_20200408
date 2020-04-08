7e92773a46981b867279442104a2a13ee896a6e9
cho881020 <kj_cho@idealidea.co.kr>
2020-04-08 21:20:12 +0900

    새 액티비티로 이름 받을 준비

---
+++ app/src/main/java/kr/tjeit/signuppractice_20200408/EditNameActivity.kt
@@ -0,0 +1,20 @@
+package kr.tjeit.signuppractice_20200408
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_name.*
+
+class EditNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_name)
+
+        okBtn.setOnClickListener {
+            val myIntent = Intent(this, MyPageActivity::class.java)
+            startActivity(myIntent)
+        }
+
+    }
+}
