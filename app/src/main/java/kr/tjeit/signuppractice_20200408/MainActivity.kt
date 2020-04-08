0f28c354e02420d169ddf6ec1279250e313cd4fb
cho881020 <kj_cho@idealidea.co.kr>
2020-04-08 21:10:06 +0900

    다음 버튼 누르면 => 마이페이지로 이동

--- app/src/main/java/kr/tjeit/signuppractice_20200408/MainActivity.kt
+++ app/src/main/java/kr/tjeit/signuppractice_20200408/MainActivity.kt
@@ -1,12 +1,24 @@
 package kr.tjeit.signuppractice_20200408
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextBtn.setOnClickListener {
+
+//            MyPageActivity로 이동.
+
+            val myIntent = Intent(this, MyPageActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
