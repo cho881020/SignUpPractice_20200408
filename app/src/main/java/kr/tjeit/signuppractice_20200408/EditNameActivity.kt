a794ffdd9b5d3ef2e8c722c1b349d4d1885d6264
cho881020 <kj_cho@idealidea.co.kr>
2020-04-08 22:07:20 +0900

    startActivityForResult 예제

--- app/src/main/java/kr/tjeit/signuppractice_20200408/EditNameActivity.kt
+++ app/src/main/java/kr/tjeit/signuppractice_20200408/EditNameActivity.kt
@@ -7,9 +7,17 @@ import kotlinx.android.synthetic.main.activity_edit_name.*
 
 class EditNameActivity : AppCompatActivity() {
 
+//    1000이라는 숫자가, 주소를 얻기위한 요청이라고 메모.
+    val REQ_FOR_ADDRESS = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_name)
+
+        inputAddressBtn.setOnClickListener {
+            val myIntent = Intent(this, EditAddressActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_ADDRESS)
+        }
 
         okBtn.setOnClickListener {
 
