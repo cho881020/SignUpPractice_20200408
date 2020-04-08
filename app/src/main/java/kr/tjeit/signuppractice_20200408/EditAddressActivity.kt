a1c81df3787e63a3405c4437896b78bbd21b5b22
cho881020 <kj_cho@idealidea.co.kr>
2020-04-08 21:58:11 +0900

    주소 입력 화면 그리기

---
+++ app/src/main/java/kr/tjeit/signuppractice_20200408/EditAddressActivity.kt
@@ -0,0 +1,12 @@
+package kr.tjeit.signuppractice_20200408
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAddressActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_address)
+    }
+}
